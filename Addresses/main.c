3f89382321346a5ee8d145f6696dc69db23e1a7f
Kate Owens <kowens711@gmail.com>
2017-04-18 11:43:51 -0400

    wrote program to det4ermine bytes that a float consumes

--- Addresses/main.c
+++ Addresses/main.c
@@ -8,9 +8,29 @@
 
 #include <stdio.h>
 
+
+void howMuchMemory(float number) {
+    
+    printf("the float consumes %zu byes\n", sizeof(number));
+}
+
 int main(int argc, const char * argv[]) {
     int i = 17;
-    printf("i stores its value at %p\n", &i);
+    int *addressOfI = &i;
+    
+    printf("i stores its value at %p\n", addressOfI);
     printf("this function starts at %p\n", main);
+    printf("the int stored at addressOfI is %d\n", *addressOfI);
+    
+    *addressOfI = 89;
+    
+    printf("Now i is %d\n", i);
+    printf("An int is %zu bytes\n", sizeof(int));
+    printf("A pointer is %zu bytes\n", sizeof(int *));
+    
+    howMuchMemory(1.000);
+    
+    
     return 0;
 }
+
